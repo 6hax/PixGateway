9daef0efa7a01a958cd465d5b8137b9588288652
haxbash <bluemaquinajp@gmail.com>
2025-08-25 19:57:44 -0300

    fix import and file name to cancel controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,4 @@
 export * from "./payment/check.js"
 export * from "./payment/create.js"
 export * from "./payment/get.js"
-export * from "./payment/webhook.js"
+export * from "./payment/cancel.js"
--- src/controllers/payment/webhook.js
+++
@@ -1,8 +0,0 @@
-export const cancelPayment = async (req, res) => {
-  try {
-    const result = await req.payment.cancelPayment(req.params.id);
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
