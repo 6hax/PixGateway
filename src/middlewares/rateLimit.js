7943d1ad62b581a2cca9551044665ae18e3d5300
haxbash <bluemaquinajp@gmail.com>
2025-08-25 19:49:36 -0300

    fix: improve rate limiting middleware

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,14 +1,13 @@
 import rateLimit from "express-rate-limit";
 
-
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100, 
+  windowMs: 15 * 60 * 1000,
+  max: 100,
   message: {
     error: "Muitas requisições, tente novamente mais tarde.",
   },
-  standardHeaders: true, 
+  standardHeaders: true,
   legacyHeaders: false,
 });
 
-export default limiter;
+export { limiter };
