f05820eaba022ae49897b57b0db10e085568e1bb
haxbash <bluemaquinajp@gmail.com>
2025-08-25 19:12:13 -0300

    change name (routes.js)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,9 +11,16 @@ import {
 const router = express.Router();
 
 router.post("/payment", authMiddleware, createPayment);
+
 router.get("/payment/:id", authMiddleware, getPayment);
 router.get("/preference/:id", authMiddleware, getPreference);
-router.get("/check/:paymentId/:preferenceId", authMiddleware, checkApproved);
+
+// router.get("/check/payment/:paymentId", authMiddleware, _); // soon
+// router.get("/check/preference/:preferenceId", authMiddleware, _); // soon
+router.get("/check/all/:paymentId/:preferenceId", authMiddleware, checkApproved);
+
 router.post("/cancel/:id", authMiddleware, cancelPayment);
+
+// router.post("/webhook", _) // soon
 
 export default router;
