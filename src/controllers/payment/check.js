03bb31aef6a6965a9d153b2b910d42e67fab763b
haxbash <bluemaquinajp@gmail.com>
2025-08-27 01:31:20 -0300

    .

--- src/controllers/payment/check.js
+++ src/controllers/payment/check.js
@@ -9,3 +9,22 @@ export const checkApproved = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+
+export const checkPayment = async (req, res) => {
+  try {
+    const result = await req.payment.checkPayment(req.params.paymentId);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
+
+export const checkPreference = async (req, res) => {
+  try {
+    const result = await req.payment.checkPreference(req.params.preferenceId);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
